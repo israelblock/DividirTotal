64acfa24c354fa74fc3ae6210b166d0480d27df0
Israel Block <israel.block@opet.edu.br>
2018-08-29 13:46:46 -0300

    Initial commit

---
+++ app/src/main/java/com/example/israe/dividirtotal/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.israe.dividirtotal;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private EditText comanda;
+    private EditText pessoas;
+    private TextView resultado;
+    private CheckBox dezporcento;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        comanda = findViewById(R.id.comanda);
+        pessoas = findViewById(R.id.pessoas);
+        resultado = findViewById(R.id.resultado);
+        dezporcento = findViewById(R.id.dezporcento);
+
+    }
+    public void calcula(View v){
+        String total = comanda.getText().toString();
+        String p = pessoas.getText().toString();
+
+        if(v.getId()==R.id.calcular){
+            if(dezporcento.isChecked()) {
+
+                float vTotal = Float.parseFloat(total);
+                float vTotal2 = (float) (vTotal + (vTotal*0.1));
+                float qPessoas = Float.parseFloat(p);
+
+                float valorPorPessoa = vTotal2 / qPessoas;
+                resultado.setText("O valor por pessoa é de "+ String.valueOf(valorPorPessoa));
+            }else {
+                float vTotal = Float.parseFloat(total);
+                float qPessoas = Float.parseFloat(p);
+
+                float valorPorPessoa = vTotal / qPessoas;
+                resultado.setText("O valor por pessoa é de "+ String.valueOf(valorPorPessoa));
+            }
+            //Toast.makeText(this, String.valueOf(valorPorPessoa), Toast.LENGTH_LONG).show();
+
+        }
+    }
+}
