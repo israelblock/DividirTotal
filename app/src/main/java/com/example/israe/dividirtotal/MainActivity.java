a7d26a81104830d532d19b07fdc3c44b249b8d82
Israel Block <israel.block@opet.edu.br>
2018-08-29 23:36:45 -0300

    Corrigido bug com campos vazios e divisões por 0

--- app/src/main/java/com/example/israe/dividirtotal/MainActivity.java
+++ app/src/main/java/com/example/israe/dividirtotal/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.DecimalFormat;
+import java.util.Objects;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +18,7 @@ public class MainActivity extends Activity {
     private EditText pessoas;
     private TextView resultado;
     private CheckBox dezporcento;
+    private Double guardaValor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,26 +29,26 @@ public class MainActivity extends Activity {
         pessoas = findViewById(R.id.pessoas);
         resultado = findViewById(R.id.resultado);
         dezporcento = findViewById(R.id.dezporcento);
+        guardaValor = null;
 
         dezporcento.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                int people = Integer.parseInt(pessoas.getText().toString());
-                if(comanda!=null && people > 0){
-                    if(isChecked==true){
+                if (!Objects.equals(comanda.getText().toString(), "")) {
+                    if (isChecked == true) {
                         double novaComanda = Double.parseDouble(comanda.getText().toString());
+                        guardaValor = novaComanda;
                         novaComanda = (novaComanda + (novaComanda * 0.1));
                         comanda.setText(new DecimalFormat(".00").format(novaComanda));
-                    }if(isChecked==false){
-                        double novaComanda = Double.parseDouble(comanda.getText().toString());
-                        novaComanda = (novaComanda - (novaComanda * 0.1));
-                        comanda.setText(new DecimalFormat(".00").format(novaComanda));
+
                     }
-                }else{
-                    Toast.makeText(MainActivity.this, "Os campos precisam ser preenchidos com valores maiores que 0", Toast.LENGTH_LONG).show();
+                    if (isChecked == false) {
+                        comanda.setText(new DecimalFormat(".00").format(guardaValor));
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "O campo de valor precisa estar preenchido ", Toast.LENGTH_LONG).show();
+                    dezporcento.setChecked(false);
                 }
-
-
             }
         });
 
@@ -57,15 +59,24 @@ public class MainActivity extends Activity {
         String p = pessoas.getText().toString();
 
         if (v.getId() == R.id.calcular) {
+            if (!Objects.equals(total, "")&& !Objects.equals(p, "") ) {
 
-            Double vTotal = Double.parseDouble(total);
-            Double qPessoas = Double.parseDouble(p);
+                Double vTotal = Double.parseDouble(total);
+                Double qPessoas = Double.parseDouble(p);
 
-            DecimalFormat mFormat = new DecimalFormat(".00");
+                DecimalFormat mFormat = new DecimalFormat(".00");
 
-            Double valorPorPessoa = vTotal / qPessoas;
+                Double valorPorPessoa = vTotal / qPessoas;
 
-            resultado.setText("O valor por pessoa é de R$" + mFormat.format(valorPorPessoa));
+                if(qPessoas == 0) {
+                    Toast.makeText(MainActivity.this, "Não é possível dividir por 0 pessoas", Toast.LENGTH_LONG).show();
+                    resultado.setText("");
+                }else {
+                    resultado.setText("O valor por pessoa é de R$" + mFormat.format(valorPorPessoa));
+                }
+            }else{
+                Toast.makeText(MainActivity.this, "Os campos precisam ser preenchidos com valores maiores que 0", Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
